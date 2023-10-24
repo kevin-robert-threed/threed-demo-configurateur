aaf418c67e0ed66a902d08788b580c1e6fc7fd27
Kévin ROBERT <kevin.robert.kr@gmail.com>
2023-10-24 10:16:47 +0200

    Fix

--- demo1/scripts/data.js
+++ demo1/scripts/data.js
@@ -2,7 +2,7 @@ const useLocalhost =
   window.location.href.includes("localhost") ||
   window.location.href.includes("127.0.0.1");
 
-const baseUrl = !useLocalhost
+const baseUrl = useLocalhost
   ? "http://localhost:8082/"
   : "https://app.threed.fr/";
 console.debug("base url : " + baseUrl);
