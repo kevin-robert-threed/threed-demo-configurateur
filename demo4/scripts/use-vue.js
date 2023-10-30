9a07488fda1caae879f7009a9a4d721725d9a241
Kévin ROBERT <kevin.robert.kr@gmail.com>
2023-10-30 18:53:15 +0100

    maj

---
+++ demo4/scripts/use-vue.js
@@ -0,0 +1,25 @@
+const App = {
+  data() {
+    return {
+      projet: projet,
+    };
+  },
+  methods: {
+    onClick(item) {
+      configureIFrame(item, this.$refs.viewer.frame);
+    },
+    getBack() {
+      debugger;
+      getBack();
+    },
+  },
+};
+
+// Create new Vue app
+const app = Vue.createApp(App);
+
+app.component("threed-titre-component", titreComponent);
+app.component("threed-viewer-component", viewerComponent);
+app.component("threed-menu-component", menuComponent);
+app.component("threed-button-component", buttonComponent);
+app.mount("#app");
