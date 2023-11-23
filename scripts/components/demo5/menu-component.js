43a699f97c575f8e23388caea4c2774885702187
Kévin ROBERT <kevin.robert.kr@gmail.com>
2023-11-23 10:16:35 +0100

    Ajout button Download

--- scripts/components/demo5/menu-component.js
+++ scripts/components/demo5/menu-component.js
@@ -38,6 +38,7 @@ const menuComponent = {
  <br>
 
 <button class="mx-2" outlined small color="primary" @click="onTest">Test</button>
+<button class="mx-2" outlined small color="primary" @click="onDownload">Download</button>
 
  `,
   data() {
@@ -69,6 +70,68 @@ const menuComponent = {
       groupe.isSelected = true;
 
       this.onTest();
+    },
+    onDownload() {
+      console.warn("onDownload");
+      console.warn(this.color);
+
+      var a = this.groupements
+        .map((groupement) => groupement.groupes)
+
+        .reduce(function (pre, cur) {
+          return pre.concat(cur);
+        })
+        .filter((groupe) => groupe.isSelected)
+        .map((groupe) => groupe.src);
+
+      var imageSrc =
+        "http://localhost:5000/images/Textures/txt_Couleur_Threed-Bleu_XXX.png";
+      if (a[0]) {
+        imageSrc = a[0];
+      }
+
+      // debugger;
+
+      const canvas = this.$refs.myCanvas;
+
+      if (canvas) {
+        const context = canvas.getContext("2d");
+        if (context) {
+          const txt = this.gravaure || "";
+
+          // context.canvas.width = context.measureText(txt).width;
+          const img = new Image();
+          img.crossOrigin = "anonymous";
+          img.addEventListener("load", (d) => {
+            // debugger;
+
+            context.canvas.width = img.width;
+            context.canvas.height = img.height;
+            // ctx.drawImage(img,0,0);
+            // ctx.font = '50px serif';
+            // ctx.fillText('Hello world', 50, 90);
+
+            const x = img.width / 4;
+            const y = img.height / 4;
+
+            context.drawImage(img, 0, 0);
+
+            context.font = "bold 100px verdana, sans-serif";
+
+            context.fillStyle = this.colorValue;
+
+            context.fillText(txt, x, y);
+            const src = context.canvas.toDataURL();
+
+            const imageUrl = src;
+            var a = document.createElement("a"); //Create <a>
+            a.href = imageUrl;
+            a.download = "Image.png"; //File name Here
+            a.click(); //Downloaded file
+          });
+          img.src = imageSrc;
+        }
+      }
     },
     onTest() {
       console.warn("test");
